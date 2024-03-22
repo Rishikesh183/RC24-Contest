c14059800fa805f0ea79ccc8791f56d7398581ae
vishalvardhan02 <124067147+vishalvardhan02@users.noreply.github.com>
2024-03-22 15:30:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 const app = express();
 
 // MongoDB connection
