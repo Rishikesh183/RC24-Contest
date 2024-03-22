6386630427b508a8c9d1dd6525863b6be9c9e28c
Rishikesh183 <158394885+Rishikesh183@users.noreply.github.com>
2024-03-22 15:34:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3001;
+const port = 3000;
 const app = express();
 
 // MongoDB connection
