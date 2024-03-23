7dc3dc649682596caf8f9e1c11b1b37d6eb35ea5
Rishikesh183 <158394885+Rishikesh183@users.noreply.github.com>
2024-03-23 18:55:30 +0530

    Update app.js
    sar sarle enno anukuntam

--- public/app.js
+++ public/app.js
@@ -10,34 +10,8 @@ document.getElementById('customForm').addEventListener('submit', function (event
         alert('Please fill in all fields.');
         event.preventDefault();
     }
-    if (Age > 50) {
-        alert("Rama Krishna ane age lo aatalu entayya");
+    if (Age > 75) {
+        alert("Enter a right age");
         event.preventDefault();
     }
 });
-
-document.getElementById('customForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const formData = new FormData(this);
-            const jsonData = {};
-            formData.forEach((value, key) => {
-                jsonData[key] = value;
-            });
-
-            fetch('/submit-form', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(jsonData)
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Handle response data
-                console.log(data);
-            })
-            .catch(error => {
-                // Handle errors
-                console.error('Error submitting form:', error);
-            });
-        });
