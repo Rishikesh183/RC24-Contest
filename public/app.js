ac6a07628c4dbb951feb88f1e5bc3bb3ebb2ca65
Rishikesh183 <158394885+Rishikesh183@users.noreply.github.com>
2024-04-13 17:02:39 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,12 +2,11 @@
 document.getElementById('customForm').addEventListener('submit', function (event) {
     var name = document.getElementById('name').value.trim();
     var email = document.getElementById('email').value.trim();
-    var message = document.getElementById('message').value.trim();
     var Age = document.getElementById('Age').value.trim();
     var phone = document.getElementById('phone').value.trim();
 
     // Simple validation
-    if (name === '' || email === '' || message === '' || Age === '' || phone === '') {
+    if (name === '' || email === '' || Age === '' || phone === '') {
         alert('Please fill in all fields.');
         event.preventDefault();
     }
